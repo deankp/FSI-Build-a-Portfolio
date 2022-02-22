563347349822fdfe9ec245cf1cd67ff99e748e45
deankp <dperez0689@gmail.com>
2022-02-22 15:48:42 -0600

    added button

---
+++ script.js
@@ -0,0 +1,6 @@
+// button to increase size of image
+document.querySelector('button').onclick = function(){
+    var image = document.getElementById('Mexico');
+    image.style.width = '25%';
+    image.style.height = 'auto';
+}
